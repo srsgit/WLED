36f2bd43cc0f1c16c7c1cffed703f3066c5bec45
srsgit <git@theddle.co.uk>
2020-02-05 18:38:11 +0000

    OTA platformio ini and some disabling of features to reduce memory

--- wled00/wled00.ino
+++ wled00/wled00.ino
@@ -18,12 +18,12 @@
 
 //You need to choose some of these features to disable:
 //#define WLED_DISABLE_ALEXA       //saves 11kb
-//#define WLED_DISABLE_BLYNK       //saves 6kb
-//#define WLED_DISABLE_CRONIXIE    //saves 3kb
-//#define WLED_DISABLE_HUESYNC     //saves 4kb
-//#define WLED_DISABLE_INFRARED    //there is no pin left for this on ESP8266-01, saves 25kb (!)
+#define WLED_DISABLE_BLYNK       //saves 6kb
+#define WLED_DISABLE_CRONIXIE    //saves 3kb
+#define WLED_DISABLE_HUESYNC     //saves 4kb
+#define WLED_DISABLE_INFRARED    //there is no pin left for this on ESP8266-01, saves 25kb (!)
 #define WLED_ENABLE_MQTT           //saves 12kb
-#define WLED_ENABLE_ADALIGHT       //saves 500b only
+// #define WLED_ENABLE_ADALIGHT       //saves 500b only
 
 #define WLED_DISABLE_FILESYSTEM    //SPIFFS is not used by any WLED feature yet
 //#define WLED_ENABLE_FS_SERVING   //Enable sending html file from SPIFFS before serving progmem version
